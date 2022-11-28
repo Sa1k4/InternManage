c43a136b09a2ba67161f206fbc2a02d4e1b0d1a3
Pumpkin <1257090532@qq.com>
2022-11-29 00:48:23 +0800

    test delete

--- src/main/java/com/example/internmanage/Entity/Test.java
+++ src/main/java/com/example/internmanage/Entity/Test.java
@@ -3,13 +3,6 @@ package com.example.internmanage.Entity;
 public class Test {
     private String test;
 
-    public String getTest() {
-        return test;
-    }
-
-    public void setTest(String test) {
-        this.test = test;
-    }
     public static void main(String[] args) {
         System.out.println("1111111111");
     }
