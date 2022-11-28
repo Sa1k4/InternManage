a3523a597b4e980196a68d01ccb27ec3cae70401
Sa1ka <760133018@qq.com>
2022-11-29 00:25:26 +0800

    test2

--- src/main/java/com/example/internmanage/Entity/Test.java
+++ src/main/java/com/example/internmanage/Entity/Test.java
@@ -3,4 +3,11 @@ package com.example.internmanage.Entity;
 public class Test {
     private String test;
 
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
 }
