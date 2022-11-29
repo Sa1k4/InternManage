41bb346bbe02dafee9acabd93457de57d4413acc
Pumpkin <1257090532@qq.com>
2022-11-29 20:05:43 +0800

    update sql

--- src/main/java/com/example/internmanage/Mapper/StudentMapper.java
+++ src/main/java/com/example/internmanage/Mapper/StudentMapper.java
@@ -1,6 +1,5 @@
 package com.example.internmanage.Mapper;
 
-import com.example.internmanage.Entity.Company;
 import com.example.internmanage.Entity.Student;
 import org.apache.ibatis.annotations.*;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,12 +15,12 @@ public interface StudentMapper {
     @Insert("insert into student (stu_id,username,password) values (#{stu_id},#{username},#{password})")
     int register(Student student);
 
-    @Update("update student set stu_id = #{stu_id},username = #{username},password = #{password},specialty = #{specialty},classname = #{classname},academy = #{academy},sex = #{sex},phone = #{phone} ")
+    @Update("update student set username = #{username},password = #{password},specialty = #{specialty},classname = #{classname},academy = #{academy},sex = #{sex},phone = #{phone} where stu_id = #{stu_id}")
     int update(Student student);
 
     @Select("select * from student")
     List<Student> selectAll();
 
-    @Delete("delete from company where stu_id = #{stu_id}")
-    int delete(int stu_id);
+    @Delete("delete from company where cpmy_id = #{cpmy_id}")
+    int delete(int cpmy_id);
 }
