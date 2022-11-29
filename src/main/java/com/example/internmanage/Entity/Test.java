2f36af21bd03d661b8f5453988dccbc72edb43be
Sa1ka <760133018@qq.com>
2022-11-29 12:27:53 +0800

    delete test

--- src/main/java/com/example/internmanage/Entity/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.internmanage.Entity;
-
-public class Test {
-    private String test;
-
-    public static void main(String[] args) {
-        System.out.println("1111111111");
-    }
-}
