de887d689c7e9ded2335b29f528d88563158c4b8
Pumpkin <1257090532@qq.com>
2022-12-06 18:44:44 +0800

    更新 5.1

---
+++ src/main/java/com/example/internmanage/Entity/TSLeave.java
@@ -0,0 +1,148 @@
+package com.example.internmanage.Entity;
+
+public class TSLeave {
+    private int stu_id;
+    private String username;
+    private String specialty;
+    private String classname;
+    private String academy;
+    private String sex;
+    private String phone;
+    private String content;
+    private String start_time;
+    private String end_time;
+    private int apply;
+    private String name;
+    private String salary;
+    private String introduce;
+    private String company_name;
+    private String company_legal;
+
+    public int getStu_id() {
+        return stu_id;
+    }
+
+    public void setStu_id(int stu_id) {
+        this.stu_id = stu_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getAcademy() {
+        return academy;
+    }
+
+    public void setAcademy(String academy) {
+        this.academy = academy;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public int getApply() {
+        return apply;
+    }
+
+    public void setApply(int apply) {
+        this.apply = apply;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getCompany_legal() {
+        return company_legal;
+    }
+
+    public void setCompany_legal(String company_legal) {
+        this.company_legal = company_legal;
+    }
+}
